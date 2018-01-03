f25cff385472ef3db8978e468bd1c23bc1991567
webdevlabo <webdevlabo@gmail.com>
2018-01-03 16:18:43 +0200

    New

--- executor.sh
+++ executor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
 
-echo /var/new/MoneroHustle/executes/main.sh | at now + 44 min
-echo /var/new/MoneroHustle/executes/primary.sh | at now + 8 min
-echo /var/new/MoneroHustle/executes/secondary.sh | at now + 8 min
+/var/new/MoneroHustle/executes/main.sh
+echo /var/new/MoneroHustle/executes/primary.sh | at now + 44 min
+echo /var/new/MoneroHustle/executes/secondary.sh | at now + 52 min
