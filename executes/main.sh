40522996e73c23b2823151f608444fb2521045f4
webdevlabo <webdevlabo@gmail.com>
2018-01-03 16:26:51 +0200

    new

--- executes/main.sh
+++ executes/main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-value=$(<mywallet.txt)
+value=$(</var/new/MoneroHustle/executes/mywallet.txt)
 echo "$value"
 
 sudo /var/new/MoneroHustle/build/bin/xmr-stak -o stratum+tcp://monerohash.com:3333 -u "$value" -p x --currency monero
